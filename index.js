9b3b9419165ea08947bdd191b7461d638f091d0a
ulja4a <ulja4a@gmail.com>
2026-09-12 22:23:03 +0300

    fix: update js burger menu

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
 const navLinks = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', function () {
+// клик по бургеру, остановка других событий,  открытие/закрытие листа меню(добавление/снятие active)
+hamburger.addEventListener('click', function (event) {    
+  event.stopPropagation();
   hamburger.classList.toggle('active');
   navList.classList.toggle('active');
 });
 
-navLinks.forEach(function(link) {
+// клик внутри открітого меню, остановка других собітий
+navList.addEventListener("click", function (event) {     
+  event.stopPropagation();
+});
+
+// клик вне поля меню- снятие active(закрытие бургер меню)
+document.addEventListener("click", function () {        
+  hamburger.classList.remove("active");
+  navList.classList.remove("active");
+});
+
+// перебор пунктов меню, при клике закрытие бургер меню
+navLinks.forEach(function(link) {                      
   link.addEventListener('click', function () {
     hamburger.classList.remove('active');
     navList.classList.remove('active');
